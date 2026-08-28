1f8a60dd6b378c9231ccc3df5145e58a3b8cf36c
cybulin.oleg <olegpipulin@mail.ru>
2026-08-28 12:35:02 +0300

    fix test navigation

--- tests/test_navigation.py
+++ tests/test_navigation.py
@@ -1,26 +1,24 @@
-from selenium.webdriver.support.ui import WebDriverWait
-
 from pages.main_page import MainPage
 from pages.dzen_page import DzenPage
 from urls import TestUrls
 
-# Проверка логотипа Яндекса, в новом окне через редирект главной страницы Дзена.
+
+# Проверка переходов по логотипам.
 class TestNavigation:
 
     def test_scooter_logo_opens_main_page(self, driver):
-        driver.get(TestUrls.BASE_URL)
-
         main_page = MainPage(driver)
 
+        main_page.go_to(TestUrls.BASE_URL)
         main_page.click_scooter_logo()
 
         assert main_page.get_current_url() == TestUrls.BASE_URL
 
-    # Тест упадет если ввести URL: https://dzen.ru/, так как сайт реализован иначе.
+    # Тест проверяет открытие Дзена в новом окне.
     def test_yandex_logo_opens_dzen(self, driver):
-        driver.get(TestUrls.BASE_URL)
-
         main_page = MainPage(driver)
+
+        main_page.go_to(TestUrls.BASE_URL)
 
         original_window = main_page.get_current_window()
 
